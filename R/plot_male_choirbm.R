3cc23c569d9451dd7f6b48a482c7c0a1a5b880c7
Eric Cramer <cramerericm@outlook.com>
2021-01-15 11:16:58 -0800

    Initial commit

--- R/plot_male_choirbm.R
+++ R/plot_male_choirbm.R
@@ -3,20 +3,19 @@
 #' Creates a new plot of the male CHOIR body map, with the option to sweitch between front and back.
 #'
 #' @param df data.frame
-#' @param face character, either 'f' or 'b' indicating front or back
 #' @return ggrob
 #' @export
 #'
 #' @examples
-plot_male_choirbm <- function(df, face = "f"){
-  datapoly <- ifelse(
-    face == "f"
-    , merge(df, male_front_poly_coords_df, by = c("id"))
-    , merge(df, male_back_poly_coords_df, by = c("id")))
+plot_male_choirbm <- function(df){
+  positions <- rbind(male_front_poly_coords_df, male_back_poly_coords_df)
+  datapoly <- merge(df, positions, by = "id")
+  print(datapoly)
   p <- ggplot2::ggplot(datapoly) +
     ggplot2::aes(x = x, y = y) +
     ggplot2::geom_polygon(ggplot2::aes(fill = value, group = id), color = "black", size = 1.5) +
     ggplot2::scale_y_reverse() +
+    ggplot2::facet_wrap(~group) +
     ggplot2::theme_void() +
     ggplot2::theme(legend.position = "None")
   return(p)
